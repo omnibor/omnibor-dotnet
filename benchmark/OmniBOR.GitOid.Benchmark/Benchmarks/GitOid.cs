9acf91608d5f80b322ab72aa2e9277874ee3f061
Jamie Magee <jamie.magee@gmail.com>
2023-01-11 13:07:24 -0800

    refactor: use unicode string literals

--- benchmark/OmniBOR.GitOid.Benchmark/Benchmarks/GitOid.cs
+++ benchmark/OmniBOR.GitOid.Benchmark/Benchmarks/GitOid.cs
@@ -1,27 +1,26 @@
 ﻿namespace GitBOM.GitOid.Benchmark.Benchmarks;
 
-using System.Text;
 using BenchmarkDotNet.Attributes;
 
 [MemoryDiagnoser]
 public class GitOid
 {
-    private static readonly byte[] DataBytes = Encoding.ASCII.GetBytes("hello World");
+    private static readonly byte[] DataBytes = "hello World"u8.ToArray();
     private static readonly Stream DataStream = new MemoryStream(DataBytes);
 
     [Benchmark]
-    public string Sha1GitOidFromBytes() => GitBOM.GitOid.GitOid
+    public string Sha1GitOidFromBytes() => OmniBOR.GitOid.GitOid
         .CreateFromBytes(HashAlgorithm.Sha1, ObjectType.Blob, DataBytes).ToString();
 
     [Benchmark]
-    public async Task<string> Sha1GitOidFromBytesAsync() => (await GitBOM.GitOid.GitOid
+    public async Task<string> Sha1GitOidFromBytesAsync() => (await OmniBOR.GitOid.GitOid
         .CreateFromBytesAsync(HashAlgorithm.Sha1, ObjectType.Blob, DataStream).ConfigureAwait(false)).ToString();
 
     [Benchmark]
-    public string Sha256GitOidFromBytes() => GitBOM.GitOid.GitOid
+    public string Sha256GitOidFromBytes() => OmniBOR.GitOid.GitOid
         .CreateFromBytes(HashAlgorithm.Sha256, ObjectType.Blob, DataBytes).ToString();
 
     [Benchmark]
-    public async Task<string> Sha256GitOidFromBytesAsync() => (await GitBOM.GitOid.GitOid
+    public async Task<string> Sha256GitOidFromBytesAsync() => (await OmniBOR.GitOid.GitOid
         .CreateFromBytesAsync(HashAlgorithm.Sha256, ObjectType.Blob, DataStream).ConfigureAwait(false)).ToString();
 }
--- test/OmniBOR.GitOid.Tests/GitOidTest.cs
+++ test/OmniBOR.GitOid.Tests/GitOidTest.cs
@@ -1,6 +1,5 @@
 ﻿namespace GitBOM.GitOid.Tests;
 
-using System.Text;
 using FluentAssertions;
 
 public class GitOidTest
@@ -8,7 +7,7 @@ public class GitOidTest
     [Fact]
     public void GenerateSha1GitOidFromBytes()
     {
-        var input = Encoding.ASCII.GetBytes("hello world");
+        var input = "hello world"u8.ToArray();
         var result = GitOid.CreateFromBytes(HashAlgorithm.Sha1, ObjectType.Blob, input);
 
         result.Hash().Should().Be("95d09f2b10159347eece71399a7e2e907ea3df4f");
@@ -18,7 +17,7 @@ public class GitOidTest
     [Fact]
     public async Task GenerateSha1GitOidFromBytesAsync()
     {
-        await using var input = new MemoryStream(Encoding.ASCII.GetBytes("hello world"));
+        await using var input = new MemoryStream("hello world"u8.ToArray());
         var result = await GitOid.CreateFromBytesAsync(HashAlgorithm.Sha1, ObjectType.Blob, input).ConfigureAwait(false);
 
         result.Hash().Should().Be("95d09f2b10159347eece71399a7e2e907ea3df4f");
@@ -28,7 +27,7 @@ public class GitOidTest
     [Fact]
     public void GenerateSha256GitOidFromBytes()
     {
-        var input = Encoding.ASCII.GetBytes("hello world");
+        var input = "hello world"u8.ToArray();
         var result = GitOid.CreateFromBytes(HashAlgorithm.Sha256, ObjectType.Blob, input);
 
         result.Hash().Should().Be("fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03");
@@ -38,7 +37,7 @@ public class GitOidTest
     [Fact]
     public async Task GenerateSha256GitOidFromBytesAsync()
     {
-        await using var input = new MemoryStream(Encoding.ASCII.GetBytes("hello world"));
+        await using var input = new MemoryStream("hello world"u8.ToArray());
         var result = await GitOid.CreateFromBytesAsync(HashAlgorithm.Sha256, ObjectType.Blob, input).ConfigureAwait(false);
 
         result.Hash().Should().Be("fee53a18d32820613c0527aa79be5cb30173c823a9b448fa4817767cc84c6f03");
